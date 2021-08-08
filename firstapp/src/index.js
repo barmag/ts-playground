dfa715d36bcd30be68b19c746a1a1b534c1f0572
Yasser Makram <yassermakram@gmail.com>
2021-08-08 06:49:43 +0200

    interfaces and classes - decalre Person interface - implement Player class - calculate age with birthdate

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+class Player {
+    name;
+    birthDate;
+    get age() {
+        if (this.birthDate) {
+            const ageTimeDiffInSeconds = Date.now() - this.birthDate.getTime() / 1000;
+            const ageTimeDiffInDays = ageTimeDiffInSeconds / (60 * 60 * 24);
+            return Math.abs(Math.round(ageTimeDiffInDays / 365.25));
+        }
+        throw new Error("Birthdate is required!");
+    }
+}
--- firstapp/src/index.ts
+++ firstapp/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+interface Person {
+    name: string;
+    birthDate: Date;
+    get age(): number;
+}
+
+class Player implements Person {
+    name: string;
+    birthDate: Date;
+    get age(): number {
+        if(this.birthDate){
+            const ageTimeDiffInSeconds = Date.now() - this.birthDate.getTime() / 1000;
+            const ageTimeDiffInDays = ageTimeDiffInSeconds / (60 * 60 * 24);
+            return Math.abs(Math.round(ageTimeDiffInDays / 365.25));
+        }
+        throw new Error("Birthdate is required!");
+    }
+    
+}
